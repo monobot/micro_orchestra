671678e6a283a69a32a1438443f5ff33bd13f76f
monobot <monobot.soft@gmail.com>
2018-07-02 21:03:16 +0200

    final version, using the ping feature to know how many instances available for each service

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # micro_orchestra
-Test for 3 microservices communicating
+##Test for 3 microservices communicating
+
+3 docker microservices communication throught a redis docker instance.
+
+Using my own library redis_connector
+
+Each of the microservices will be able to process its own messages, and will be connecting to the list of channels.
+Each message will be processed only once using the redis lock feature.
+So you can scalate up any of the microservices without worring about the concurrence.
+
+eg:
+docker-compose up --scale final__box=3 processor__box=2
--- publisher_box/start.py
+++ publisher_box/start.py
@@ -1,93 +1,7 @@
 #!/usr/bin/env python
-import json
-import redis
-import time
 import uuid
 
-
-class RedisConnector:
-
-    def __init__(self, host, port, queues, service_name):
-        """Init method for RedisConnector
-
-        :param host: the redis host yu will connect to.
-        :type host: str
-
-        :param port: redis port.
-        :type port: int
-
-        :param queues: the name of the redis queue you need to subscribe to.
-        :type queues: string
-
-        :param service_name: The name of the service is connecting to the queue.
-        :type service_name: type
-
-        :rtype: None
-        """
-        self._redis = redis.StrictRedis(host=host, port=port)
-        self.pub_sub = self._redis.pubsub()
-        self.pub_sub.subscribe(queues)
-        self.service_name = service_name
-
-    def __enter__(self):
-        """Implement with statement."""
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        """Implement with statement."""
-        return self.close()
-
-    def close(self):
-        self.pub_sub.close()
-
-    def publish(self, queue_name, message):
-        """Will publish the message to the queue you are subscribed to.
-
-        :param message: message you want to publish.
-        :type message: str
-
-        :rtype: None
-        """
-        self._redis.publish(queue_name, message)
-
-    def subscribe(self):
-        """Subscribes to the redis queue.
-
-        :rtype: None
-        """
-        message = self.pub_sub.get_message()
-        if message:
-            data = message['data']
-            if message['type'] == 'message':
-                data = json.loads(data.decode('utf8').replace("'", '"'))
-                # No target means promiscuous mode for that message, no lock will be used
-                if data.pop('target') is None:
-                    self._process(data)
-                elif data.pop('target') == self.service_name:
-                    if self._acquire_lock(data['message_id']):
-                        self._process(data)
-
-    def _acquire_lock(self, message_id, lock_timeout=10):
-        """Will lock the redis key for a max of 10 seconds,
-        if the lock is free then it will process the message inmediatly, leaving it locked for the lock_timeout in all
-        the cases, thus not allowing to process the messege to any other service.
-
-        :rtype: bool
-        """
-        expire_time = time.time() + (lock_timeout / 2)
-        while time.time() < expire_time:
-            if self._redis.setnx(message_id, expire_time):
-                self._redis.expire(message_id, lock_timeout)
-                return True
-
-            elif not self._redis.ttl(message_id):
-                self._redis.expire(message_id, lock_timeout)
-
-            time.sleep(0.001)
-        return False
-
-    def _process(self, message):
-        print(message)
+from redis_connector import RedisConnector
 
 
 HOST = 'redis_cache'
@@ -96,7 +10,7 @@ QUEUENAME = 'microservices'
 QUEUES = [QUEUENAME, ]
 MICRO_SERVICE_NAME = 'publisher'
 
-redis_subscriber = RedisConnector(HOST, PORT, QUEUES, MICRO_SERVICE_NAME)
+redis_connector_publisher = RedisConnector(HOST, PORT, QUEUES, MICRO_SERVICE_NAME)
 t = 1
 while True:
     message = {
@@ -109,6 +23,20 @@ while True:
             'second': t,
         }
     }
-    print('publishing {}'.format(message))
-    redis_subscriber.publish(QUEUENAME, json.dumps(message))
+    redis_connector_publisher.publish(QUEUENAME, message)
     t += 1
+    processor_instances = redis_connector_publisher.publish(
+        'ping:processor',
+        {"message": "ping"}
+    )
+    final_instances = redis_connector_publisher.publish(
+        'ping:final',
+        {"message": "ping"}
+    )
+    publisher_instances = redis_connector_publisher.publish(
+        'ping:publisher',
+        {"message": "ping"}
+    )
+    print('publisher_instances: {}'.format(publisher_instances))
+    print('processor_instances: {}'.format(processor_instances))
+    print('final_instances: {}'.format(final_instances))
